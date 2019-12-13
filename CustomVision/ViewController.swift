46b8ddc718780346db29e990da1ed29a3bcf95d3
MinGyu, Ju <knighp@gmail.com>
2019-12-13 16:10:33 +0900

    Initial Commit

---
+++ CustomVision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomVision
+//
+//  Created by MinGyu Ju on 13/12/2019.
+//  Copyright © 2019 RecursiveSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
